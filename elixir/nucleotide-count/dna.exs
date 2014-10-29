2b0fc6d45bc4512760924a7b01a23f9e44879080
Eduardo Borges <eduardomrb@gmail.com>
2014-10-28 22:31:46 -0300

    submits nucleotide_count (Elixir)

---
+++ elixir/nucleotide-count/dna.exs
@@ -0,0 +1,40 @@
+defmodule DNA do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a DNA strand.
+
+  ## Examples
+
+  iex> DNA.count('AATAA', ?A)
+  4
+
+  iex> DNA.count('AATAA', ?T)
+  1
+  """
+  @spec count([char], char) :: non_neg_integer
+  def count(strand, nucleotide) do
+    strand |> Enum.filter(&(&1 == nucleotide)) |> Dict.size
+  end
+
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> DNA.nucleotide_counts('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec nucleotide_counts([char]) :: Dict.t
+  def nucleotide_counts(strand) do
+    base_count = %{?A => 0, ?T => 0, ?C => 0, ?G => 0}
+    nucleotide_counts(strand, base_count)
+  end
+
+  def nucleotide_counts([], acc), do: acc
+  def nucleotide_counts([head | tail], acc) do
+    new_acc = Dict.update(acc, head, 1, &(&1 + 1))
+    nucleotide_counts(tail, new_acc)
+  end
+end
