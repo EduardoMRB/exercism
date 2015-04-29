4c8224130718624a67b7295a4caf3808209028b5
Eduardo Borges <eduardomrb@gmail.com>
2015-04-29 17:40:30 -0400

    First iteration on Gigasecond (Clojure)

---
+++ clojure/gigasecond/project.clj
@@ -0,0 +1,7 @@
+(defproject gigasecond "0.1.0-SNAPSHOT"
+  :description "gigasecond exercise."
+  :url "https://github.com/exercism/xclojure/tree/master/gigasecond"
+  :source-paths [""]
+  :test-paths [""]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-time "0.9.0"]])
