8afa6ff9774f831ee81e86c16e8eb7ec773a59b7
Eduardo Borges <eduardomrb@gmail.com>
2015-05-01 00:23:28 -0400

    First iteration on bob (Python)

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,6 +1,26 @@
-#
-# Skeleton file for the Python "Bob" exercise.
-#
-def hey(what):
+from re import compile
 
-    return
+def shout(sentence):
+    letters_pattern = compile("[a-zA-Z]")
+    letters = filter(lambda letter: letters_pattern.match(letter), sentence)
+    res = map(lambda letter: letter.upper() == letter, letters)
+    if len(res) > 0:
+        return all(res)
+    else:
+        return False
+
+def question(sentence):
+    return sentence.strip().endswith("?")
+
+def blank(sentence):
+    return sentence.strip() == ""
+
+def hey(what):
+    if shout(what):
+        return "Whoa, chill out!"
+    elif question(what):
+        return "Sure."
+    elif blank(what):
+        return "Fine. Be that way!"
+    else:
+        return "Whatever."
