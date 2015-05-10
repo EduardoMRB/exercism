572919abc56392fd009e01c8da3a24cedc058f07
Eduardo Borges <eduardomrb@gmail.com>
2015-05-09 20:11:59 -0400

    First iteration on Raindrops (Clojure)
    It's very ugly and I stole part of it's functionality from prime-factors
    exercise. I'm going to clean this up on next iterations.

---
+++ clojure/raindrops/raindrops.clj
@@ -0,0 +1,30 @@
+(ns raindrops)
+
+(defn divisible-by [divisor dividend]
+  (zero? (mod dividend divisor)))
+
+(defn prime-factors-of [n]
+  (loop [num     n
+         divisor 2
+         factors []]
+    (cond
+      (<= num 1) factors
+      (divisible-by divisor num) (recur (/ num divisor)
+                                        divisor
+                                        (conj factors divisor))
+      :else (recur num (inc divisor) factors))))
+
+(defn convert [n]
+  (let [prime-factors (set (prime-factors-of n))
+        mappings      [[3 "Pling"]
+                       [5 "Plang"]
+                       [7 "Plong"]]
+        res           (reduce (fn [res [fac text]]
+                                (if (contains? prime-factors fac)
+                                  (str res text)
+                                  res))
+                              ""
+                              mappings)]
+    (if (seq res)
+      res
+      (str n))))
