121dd44e88c00d4e8c7ba76916eed9e8ed24556a
Eduardo Borges <eduardomrb@gmail.com>
2015-05-20 08:24:44 -0400

    Second iteration RNA Transcription (Python)
    Switch string concatenation for `str.join` for it's performance is
    surprisingly better.

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,4 +1,4 @@
 DNA_TO_RNA = {"A":"U", "T":"A", "C":"G", "G":"C"}
 
 def to_rna(dna):
-    return reduce(lambda acc, strand: acc + DNA_TO_RNA[strand], dna, "")
+    return "".join(map(lambda strand: DNA_TO_RNA[strand], dna))
