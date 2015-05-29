bcf06dc087b3501e14c7d6ce7bad60975c5ec8eb
Eduardo Borges <eduardomrb@gmail.com>
2015-05-29 12:58:53 -0400

    First iteration on Grade School (Elixir)

---
+++ elixir/grade-school/school.exs
@@ -0,0 +1,31 @@
+defmodule School do
+  @moduledoc """
+  Simulate students in a school.
+
+  Each student is in a grade.
+  """
+
+  @doc """
+  Add a student to a particular grade in school.
+  """
+  @spec add(Dict.t, String.t, pos_integer) :: Dict.t
+  def add(db, name, grade) do
+    Dict.update(db, grade, [name], fn(val) -> [name | val] end)
+  end
+
+  @doc """
+  Return the names of the students in a particular grade.
+  """
+  @spec grade(Dict.t, pos_integer) :: [String]
+  def grade(db, grade) do
+    Dict.get(db, grade, [])
+  end
+
+  @doc """
+  Sorts the school by grade and name.
+  """
+  @spec sort(Dict) :: Dict.t
+  def sort(db) do
+    db
+  end
+end
