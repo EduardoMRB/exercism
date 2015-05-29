549d6815d20faadb0cd816fe931bc50f168de19a
Eduardo Borges <eduardomrb@gmail.com>
2015-05-29 13:44:01 -0400

    First attempt on Anagram (Scheme)

--- scheme/anagram/anagram.scm
+++ scheme/anagram/anagram.scm
@@ -1,2 +1,15 @@
 (define-module (anagram)
   #:export (anagrams-for))
+
+(define (anagrams-for word candidates)
+  (filter (lambda (candidate)
+            (= (sort-string word)
+               (sort-string candidate)))
+          candidates))
+
+
+(define (sort-string str)
+  (let ((string-list (string->list str)))
+    (apply string (map integer->char
+                       (sort (map char->integer string-list)
+                             <)))))
