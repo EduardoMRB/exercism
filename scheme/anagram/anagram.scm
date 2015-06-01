ab69b7b0d148ed066505941767aafaa4b6024498
Eduardo Borges <eduardomrb@gmail.com>
2015-06-01 13:32:56 -0400

    First iteration on Anagram (Scheme)

--- scheme/anagram/anagram.scm
+++ scheme/anagram/anagram.scm
@@ -3,13 +3,17 @@
 
 (define (anagrams-for word candidates)
   (filter (lambda (candidate)
-            (= (sort-string word)
-               (sort-string candidate)))
+            (let ((word (string-downcase word))
+                  (candidate (string-downcase candidate)))
+              (and (equal? (sort-string word)
+                           (sort-string candidate))
+                   (not (equal? candidate word)))))
           candidates))
 
 
 (define (sort-string str)
   (let ((string-list (string->list str)))
-    (apply string (map integer->char
-                       (sort (map char->integer string-list)
-                             <)))))
+    (apply string
+           (map integer->char
+                (sort (map char->integer string-list)
+                      <)))))
