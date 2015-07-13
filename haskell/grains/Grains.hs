d09c87bf156b583f7da34e4b62d941788ff88af9
Eduardo Borges <eduardomrb@gmail.com>
2015-07-13 13:11:55 -0400

    Using foldl' instead of foldl (Grains - Haskell)

--- haskell/grains/Grains.hs
+++ haskell/grains/Grains.hs
@@ -1,7 +1,9 @@
 module Grains where
 
+import Data.List(foldl')
+
 square :: Integer -> Integer
 square n = 2 ^ (n - 1)
 
 total :: Integer
-total = foldl (+) 0 $ take 64 $ iterate (* 2) 1
+total = foldl' (+) 0 $ take 64 $ iterate (* 2) 1
