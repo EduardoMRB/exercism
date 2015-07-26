4f8e6d5681011ea7a4c579d061215271a6952a67
Eduardo Borges <eduardomrb@gmail.com>
2015-07-26 18:44:17 -0400

    First iteration on Binary Search Tree (Clojure)

---
+++ clojure/binary-search-tree/binary_search_tree.clj
@@ -0,0 +1,38 @@
+(ns binary-search-tree)
+
+(defn singleton [v]
+  {:left  nil
+   :right nil
+   :value v})
+
+(defn value [node]
+  (:value node))
+
+(defn left [node]
+  (:left node))
+
+(defn right [node]
+  (:right node))
+
+(defn insert [v node]
+  (let [direction (if (<= v (value node))
+                    :left
+                    :right)]
+    (if (nil? (direction node))
+      (assoc node direction (singleton v))
+      (assoc node direction (insert v (direction node))))))
+
+
+(defn from-list [coll]
+  (reduce (fn [node v]
+            (if (seq node)
+              (insert v node)
+              (singleton v)))
+          nil
+          coll))
+
+(defn to-list [tree]
+  (lazy-cat
+   (when-not (nil? (left tree)) (to-list (left tree)))
+   (vector (value tree))
+   (when-not (nil? (right tree)) (to-list (right tree)))))
